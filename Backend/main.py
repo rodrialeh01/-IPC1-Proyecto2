19d621275012e35bc5af8b235f8c73ddea1ea8ab
Rodrigo Hernández <2823897250101@ingenieria.usac.edu.gt>
2021-10-11 23:31:50 -0600

    CRUD de Usuarios
    Ya puede realizar las operaciones del CRUD de usuarios aunque falta validarlo en el frontend

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
+import re
 
 #Llamar a la clase
 from Usuario import Usuario
@@ -14,6 +15,11 @@ CORS(app)
 Usuarios = []
 
 Usuarios.append(Usuario("Abner Cardona","M","admin","admin@ipc1.com","admin@ipc1"))
+
+#METODO PARA MOSTRAR AL INICIO DEL BACKEND
+@app.route('/',methods=['GET'])
+def Inicio():
+    return("<h1>Backend U-Blog</h1>")
 
 #METODO PARA CREAR USUARIOS
 @app.route('/Usuarios/Registrar', methods=['POST'])
@@ -28,9 +34,12 @@ def RegistrarUsuario():
 
         if VerificarUsuario(user) == False:
             if cantidadpassword(contraseña) == True:
-                nuevo = Usuario(nombre,genero,user,email,contraseña)
-                Usuarios.append(nuevo)
-                return jsonify({'Mensaje': 'Se agregó el usuario correctamente'})
+                if genero == "M" or genero =="F" or genero=="m" or genero=="f":
+                    nuevo = Usuario(nombre,genero.upper(),user,email,contraseña)
+                    Usuarios.append(nuevo)
+                    return jsonify({'Mensaje': 'Se agregó el usuario correctamente'})
+                else:
+                    return jsonify({'Mensaje': 'Unicamente puede poner "M", "m", "F" o "f" en el genero'})
             else:
                 return jsonify({'Mensaje': 'Su contraseña debe ser mayor a 8 caracteres'})               
         else:
@@ -50,13 +59,6 @@ def VerificarUsuario(nusuario):
             else:
                 validar =False
         return validar
-
-#METODO PARA VERIFICAR LA LONGITUD DE LA CONTRASEÑA
-def cantidadpassword(passw):
-    if len(passw) >=8:
-        return True
-    else:
-        return False
 
 #METODO PARA VER LOS USUARIOS
 @app.route('/Usuarios', methods=['GET'])
@@ -95,6 +97,62 @@ def VerificarUsuarios(us,contra):
         else:
             validar = False
     return validar
+
+#MODIFICAR UN USUARIO
+@app.route('/Usuarios/Modificar/<string:user>',methods=['PUT'])
+def ActualizarUsuario(user):
+    global Usuarios
+    for i in range(len(Usuarios)):
+        if user == Usuarios[i].getUser():
+            nombre = request.json['name']
+            genero = request.json['gender']
+            correo = request.json['email']
+            contraseña = request.json['password']
+            if cantidadpassword(contraseña) == True:
+                if genero == "M" or genero =="F" or genero=="m" or genero=="f":
+                    Usuarios[i].setNombre(nombre)
+                    Usuarios[i].setGenero(genero)
+                    Usuarios[i].setCorreo(correo)
+                    Usuarios[i].setContraseña(contraseña)
+                else:
+                    return jsonify({'Mensaje': 'Unicamente puede poner "M", "m", "F" o "f" en el genero'})
+            else:
+                return jsonify({'Mensaje': 'Su contraseña debe ser mayor a 8 caracteres'})
+        else:
+            return jsonify({'Mensaje': 'No se encontró el usuario'})
+
+#METODO PARA RETORNAR 1 USUARIO
+@app.route('/Usuarios/<string:user>', methods=['GET'])
+def RetornarUsuario(user):
+    global Usuarios
+    for Usuario in Usuarios:
+        if Usuario.getUser():
+            objeto = {
+                'name': Usuario.getName(),
+                'user': Usuario.getUser(),
+                'password': Usuario.getContraseña(),
+                'gender': Usuario.getGenero(),
+                'email': Usuario.getCorreo()
+            }
+            return(jsonify(objeto))
+    return(jsonify({'Mensaje':'No se encuentra el usuario'}))
+
+#METODO PARA VERIFICAR LA LONGITUD DE LA CONTRASEÑA
+def cantidadpassword(passw):
+    if len(passw) >=8:
+        return True
+    else:
+        return False
+
+#METODO PARA ELIMINAR UN USUARIO
+@app.route('/Usuarios/Eliminar/<string:user>',methods=['DELETE'])
+def EliminarUsuario(user):
+    global Usuarios
+    for i in range(len(Usuarios)):
+        if user == Usuarios[i].getUser():
+            del Usuarios[i]
+            return jsonify({'Mensaje':'Se eliminó el usuario exitosamente'})
+    return jsonify({'Mensaje':'No se encontró el usuario'})
 
 #Hace que se levante la api que se esta creando
 if __name__ == "__main__":
